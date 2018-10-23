0db5df0ec3ce99148f0f18b45b3cbcebf883ebcb
Conor Hamilton <conhamil19@gmail.com>
2018-10-23 17:42:52 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -13,7 +13,8 @@ class FiguresController < ApplicationController
     @figure = Figure.create(params[:figure])
     if !params["title"]["name"].empty?
       @figure.title << Title.create(params[:title])
-    
+    end 
+    if !
   
   end 
 end
